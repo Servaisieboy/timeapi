d2e05683249573b629faf0b39f024efd4c3032a9
servais <37021689+Servaisieboy@users.noreply.github.com>
2024-12-04 18:25:11 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,13 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify
 from datetime import datetime
 import pytz
 
 app = Flask(__name__)
 
 def get_current_time(city):
-    """
-    Haal de huidige tijd op voor een specifieke stad.
-    """
     timezones = {
         "New York": "America/New_York",
         "Porto": "Europe/Lisbon"
@@ -18,19 +15,13 @@ def get_current_time(city):
 
 @app.route("/api/time/newyork", methods=["GET"])
 def time_new_york():
-    """
-    API-endpoint om de tijd in New York te retourneren als platte tekst.
-    """
     time = get_current_time("New York")
-    return time, 200, {'Content-Type': 'text/plain'}
+    return jsonify({"time": time})
 
 @app.route("/api/time/porto", methods=["GET"])
 def time_porto():
-    """
-    API-endpoint om de tijd in Porto te retourneren als platte tekst.
-    """
     time = get_current_time("Porto")
-    return time, 200, {'Content-Type': 'text/plain'}
+    return jsonify({"time": time})
 
-# Maak de app beschikbaar voor Vercel
+# Export the app as a callable for Vercel
 app = app
